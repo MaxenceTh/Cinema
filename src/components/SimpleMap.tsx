aa61e13b22911c20e5e4bfaf497daa316b3fd30c
MaxenceTh <thomas.maxence141v2@gmail.com>
2025-02-18 18:27:40 +0100

    update

--- src/components/SimpleMap.tsx
+++ src/components/SimpleMap.tsx
@@ -9,6 +9,16 @@ interface Cinema {
     nom: string;
     latitude: number;
     longitude: number;
+    commune: string;
+    multiplexe: string;
+    population_commune: string;
+    ecrans: string;
+    fauteuils: string;
+    entrees_2021: number;
+    entrees_2022: number;
+    evolution_entrees: number;
+    adresse: string;
+
 }
 
 // Définition des props attendues
@@ -17,17 +27,18 @@ interface SimpleMapProps {
 }
 
 const SimpleMap = ({ region }: SimpleMapProps) => {
-    
+
     const mapRef = useRef(null);
     const latitude = 48.0047647109243;
     const longitude = -4.091081280049094;
 
     const [cinemas, setCinemas] = useState<Cinema[]>([]);
+    const [selectedCinema, setSelectedCinema] = useState<Cinema | null>(null); // Stocke le cinéma sélectionné
 
     useEffect(() => {
         if (region) {
             const baseURL = `http://localhost:8080/cinemas/region?region=${region}`;
-            
+
             axios.get(baseURL)
                 .then((response) => {
                     setCinemas(response.data);
@@ -40,21 +51,52 @@ const SimpleMap = ({ region }: SimpleMapProps) => {
     }, [region]); // Met à jour les cinémas lorsque la région change
 
     return (
-        <MapContainer center={[latitude, longitude]} zoom={7} ref={mapRef} style={{ height: "70vh", width: "100%" }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+        <>
+            <div className="flex">
+                <MapContainer center={[latitude, longitude]} zoom={7} ref={mapRef} style={{ height: "70vh", width: "100%" }}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
 
-            {/* Ajout dynamique des markers */}
-            {cinemas.map((cinema) => (
-                <Marker key={cinema.id} position={[cinema.latitude, cinema.longitude]}>
-                    <Popup>
-                        <strong>{cinema.nom}</strong>
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+                    {/* Ajout dynamique des markers */}
+                    {cinemas.map((cinema) => (
+                        <Marker key={cinema.id} position={[cinema.latitude, cinema.longitude]}
+                            eventHandlers={{
+                                click: () => setSelectedCinema(cinema), // Met à jour l'état au clic
+                            }}>
+                            <Popup>
+                                <p>Nom du cinéma: <strong>{cinema.nom}</strong></p>
+                                <p>Multiplexe: <strong>{cinema.multiplexe}</strong></p>
+                                <p>Commune: <strong>{cinema.commune}</strong></p>
+                                <p>Population commune: <strong>{cinema.population_commune}</strong></p>
+                                <p>Fauteuils: <strong>{cinema.fauteuils}</strong></p>
+                                <p>Écrans: <strong>{cinema.ecrans}</strong></p>
+                            </Popup>
+                        </Marker>
+                    ))}
+                </MapContainer>
+
+                {/* Affichage des infos du cinéma sélectionné */}
+                {selectedCinema && (
+                    <div className="text-center m-auto">
+                        <h3 className="text-2xl">Informations sur le cinéma</h3>
+                        <p><strong>Nom :</strong> {selectedCinema.nom}</p>
+                        <p><strong>Adresse :</strong> {selectedCinema.adresse}</p>
+                        <p><strong>Commune :</strong> {selectedCinema.commune}</p>
+                        <p><strong>Multiplexe :</strong> {selectedCinema.multiplexe}</p>
+                        <p><strong>Population de la commune :</strong> {selectedCinema.population_commune}</p>
+                        <p><strong>Écrans :</strong> {selectedCinema.ecrans}</p>
+                        <p><strong>Fauteuils :</strong> {selectedCinema.fauteuils}</p>
+                        <p><strong>Entrées en 2021 :</strong> {selectedCinema.entrees_2021}</p>
+                        <p><strong>Entrées en 2022 :</strong> {selectedCinema.entrees_2022}</p>
+                        <p><strong>Évolution des entrées :</strong> {selectedCinema.evolution_entrees}%</p>
+                        <p><strong>Latitude :</strong> {selectedCinema.latitude}</p>
+                        <p><strong>Longitude :</strong> {selectedCinema.longitude}  </p>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
